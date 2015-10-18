8f49a585656a830bb8b19662056d58ae734f4a82
David Colclazier <david@jdc.tech>
2015-10-18 01:13:26 -0600

    Pushing compressed file containing all of the customized libraries.

--- external_temperature_actions.cpp
+++ external_temperature_actions.cpp
@@ -8,12 +8,9 @@ SETUP_ACTION(get_external_temp) : oneWireBus(OneWire(2)), sensors(&oneWireBus) {
 }
 
 EXECUTE_ACTION(get_external_temp) {
-	/*externalTemp_args * temp_args = static_cast<externalTemp_args*>(args);*/
 
 	sensors.requestTemperatures();//Request reading from probe
 	_args.EXT_Temp = sensors.getTempCByIndex(0);
 	EVENTHANDLER.trigger("external_temp_update", &_args);
-								  
-								  //temp_args->EXT_Temp = sensors.getTempCByIndex(0);//set temp argument "Ext_Temp" to probe reading
 }
 
--- external_temperature_actions.h
+++ external_temperature_actions.h
@@ -4,7 +4,7 @@
 #include <DallasTemperature.h>
 
 CREATE_EVENT_TRIGGERING_ACTION(get_external_temp, externalTemp_args)
-//#define ONE_WIRE_BUS 2 //Assigns IC Bus to pin #2
+
 OneWire oneWireBus;
 DallasTemperature sensors;
 
