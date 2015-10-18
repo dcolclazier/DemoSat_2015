474f8ddb9db896e82ec9fad4b0fb7fd99aa01851
Wesley <Wesley@Wesleys-MacBook-Pro.local>
2015-10-17 21:28:07 -0600

    Beginning of EXT temp

---
+++ external_temperature_actions.cpp
@@ -0,0 +1,15 @@
+
+#include "external_temperature_actions.h"
+#include "EventAction.h"
+
+SETUP_SIMPLEACTION(get_external_temp) {
+	sensors.begin();//Turn on all sensors on IC bus
+}
+
+EXECUTE_ACTION(get_external_temp) {
+	temperature_args * temp_args = static_cast<temperature_args*>(args);
+
+	sensors.requestTemperatures();//Request reading from probe
+	temp_args->EXT_Temp = sensors.getTempCByIndex(0);//set temp argument "Ext_Temp" to probe reading
+}
+
---
+++ external_temperature_actions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "EventHandler.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+CREATE_SIMPLEACTION(get_external_temp)
+#define ONE_WIRE_BUS 2 //Assigns IC Bus to pin #2
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any "OneWire" devices on the bus
+DallasTemperature sensors(&oneWire); // Pass oneWire as reference to DallasTemperature
+END_CREATE
+
